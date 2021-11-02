4a672550371ccd5730b49a9b2c28fa7bcb232b46
xuchaofeng <826639991@qq.com>
2021-11-02 20:36:31 +0800

    碰撞逻辑写法优化

--- src/main/java/game/facade/WallBulletCollision.java
+++ src/main/java/game/facade/WallBulletCollision.java
@@ -9,18 +9,16 @@ public class WallBulletCollision implements Collision {
     @Override
     public boolean checkCollision(GameObject o1, GameObject o2) {
         if (o1 instanceof Wall && o2 instanceof Bullet) {
-            return collisionDetect((Bullet)o2, (Wall)o1);
+            Bullet b = (Bullet) o2;
+            Wall w = (Wall) o1;
+            if (b.getRectangle().intersects(w.getRectangle())) {
+                b.getGm().list.remove(o2);
+            }
+            return false;
         } else if (o2 instanceof Wall && o1 instanceof Bullet) {
-            return collisionDetect((Bullet)o1, (Wall)o2);
+            return checkCollision(o1,o2);
         } else {
             return false;
         }
-    }
-
-    private boolean collisionDetect(Bullet o2, Wall o1) {
-        if (o2.getRectangle().intersects(o1.getRectangle())) {
-            o2.getGm().list.remove(o2);
-        }
-        return false;
     }
 }
